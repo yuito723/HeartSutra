255bc5f96979d61827105ce8c209cb7f197d17f3
cyber-yuito723 <cyber.yuito723@icloud.com>
2023-12-16 23:52:52 +0900

    20231216

--- run.py
+++ run.py
@@ -8,17 +8,76 @@ import pyperclip
 import subprocess
 import time
 
-print("３秒後にメモ帳が起動した後、５秒後に写経を開始します。")
+print("cyber-yuito723/HeartSutra\n(C) 2023 cyber-yuito723\n\nメモ帳が起動した後、３秒後に写経がはじまります。")
 time.sleep(3)
 subprocess.Popen("notepad.exe")
-time.sleep(5)
+time.sleep(3)
 
 def display(letter):
     pyperclip.copy(letter)
     pyautogui.hotkey("ctrl", "v")
     time.sleep(0.5)
 
-a = ["あ", "い", "う"]
+script_1 = ("観", "自", "在", "菩", "薩", "　", "行", "深", "般", "若", "波", "羅", "蜜", "多", "時")
+script_2 = ("照", "見", "五", "蘊", "皆", "空", "　", "度", "一", "切", "苦", "厄")
+script_3 = ("舎", "利", "子", "　", "色", "不", "異", "空", "　", "空", "不", "異", "色")
+script_4 = ("色", "即", "是", "空", "　", "空", "即", "是", "色")
+script_5 = ("受", "想", "行", "識", "　", "亦", "復", "如", "是")
+script_6 = ("舎", "利", "子", "　", "是", "諸", "法", "空", "相")
+script_7 = ("不", "生", "不", "滅", "　", "不", "垢", "不", "浄", "　", "不", "増", "不", "減")
+script_8 = ("是", "故", "空", "中", "　", "無", "色", "無", "受", "想", "行", "識")
+script_9 = ("無", "眼", "耳", "鼻", "舌", "身", "意", "　", "無", "色", "声", "香", "味", "触", "法")
+script_10 = ("無", "眼", "界", "　", "乃", "至", "無", "意", "識", "界")
+script_11 = ("無", "無", "明", "　", "亦", "無", "無", "明", "尽")
+script_12 = ("乃", "至", "無", "老", "死", "　", "亦", "無", "老", "死", "尽")
+script_13 = ("無", "苦", "集", "滅", "道", "　", "無", "智", "亦", "無", "得", "　", "以", "無", "所", "得", "故")
+script_14 = ("菩", "提", "薩", "埵", "　", "依", "般", "若", "波", "羅", "蜜", "多", "故")
+script_15 = ("心", "無", "罣", "礙", "　", "無", "罣", "礙", "故", "　", "無", "有", "恐", "怖")
+script_16 = ("遠", "離", "一", "切", "顛", "到", "夢", "想", "　", "究", "境", "涅", "槃")
+script_17 = ("三", "世", "諸", "仏", "　", "依", "般", "若", "波", "羅", "蜜", "多", "故")
+script_18 = ("得", "阿", "耨", "多", "羅", "三", "藐", "三", "菩", "提")
+script_19 = ("故", "知", "般", "若", "波", "羅", "蜜", "多")
+script_20 = ("是", "大", "神", "呪", "　", "是", "大", "明", "呪")
+script_21 = ("是", "無", "上", "呪", "　", "是", "無", "等", "等", "呪")
+script_22 = ("能", "除", "一", "切", "苦", "　", "真", "実", "不", "虚")
+script_23 = ("故", "説", "般", "若", "波", "羅", "蜜", "多", "呪")
+script_24 = ("即", "説", "呪", "曰")
+script_25 = ("羯", "諦", "　", "羯", "諦", "　", "波", "羅", "羯", "諦", "　", "波", "羅", "僧", "羯", "諦")
+script_26 = ("菩", "提", "薩", "婆", "訶")
+script_27 = ("般", "若", "心", "経")
 
-for i in a:
-    display(i)
+for i in range(1, 27):
+    pass
+
+
+
+for j in script_1:
+    display(j)
+
+pyautogui.press("enter", presses = 2)
+
+pyautogui.press("enter", presses = 2)
+
+pyautogui.press("enter", presses = 2)
+
+pyautogui.press("enter", presses = 2)
+
+pyautogui.press("enter", presses = 2)
+
+pyautogui.press("enter", presses = 2)
+
+pyautogui.press("enter", presses = 2)
+
+pyautogui.press("enter", presses = 2)
+
+pyautogui.press("enter", presses = 2)
+
+pyautogui.press("enter", presses = 2)
+
+pyautogui.press("enter", presses = 2)
+
+pyautogui.press("enter", presses = 2)
+
+pyautogui.press("enter", presses = 2)
+
+
